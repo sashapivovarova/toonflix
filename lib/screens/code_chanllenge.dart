f6c8845992534e851ad6fd1d154c7b3770f074ed
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-07-28 23:46:09 +0900

    codeChanllenge2

--- lib/screens/code_chanllenge.dart
+++ lib/screens/code_chanllenge.dart
@@ -1,59 +1,57 @@
-typedef LookupFunction = String Function(String term);
+typedef Settings = Map<String, String>;
 
 class Dictionary {
-  final Map<String, String> _data = {};
+  final Settings word = {};
 
   void add(String term, String definition) {
-    _data[term] = definition;
+    word[term] = definition;
   }
 
   String get(String term) {
-    return _data[term] ?? "Definition not found";
+    return word[term] ?? "Term was not found";
   }
 
   void delete(String term) {
-    _data.remove(term);
+    word.remove(term);
   }
 
   void update(String term, String definition) {
-    if (_data.containsKey(term)) {
-      _data[term] = definition;
-    } else {
-      print("Term '$term' not found in the dictionary.");
+    if (word.containsKey(term)) {
+      word[term] = definition;
     }
   }
 
   void showAll() {
-    _data.forEach((term, definition) {
+    word.forEach((term, definition) {
       print('$term: $definition');
     });
   }
 
   int count() {
-    return _data.length;
+    return word.length;
   }
 
   void upsert(String term, String definition) {
-    _data[term] = definition;
+    word[term] = definition;
   }
 
   bool exists(String term) {
-    return _data.containsKey(term);
+    return word.containsKey(term);
   }
 
-  void bulkAdd(List<Map<String, String>> terms) {
+  void bulkAdd(List<Settings> terms) {
     for (var termData in terms) {
       final term = termData['term'];
       final definition = termData['definition'];
       if (term != null && definition != null) {
-        _data[term] = definition;
+        word[term] = definition;
       }
     }
   }
 
   void bulkDelete(List<String> terms) {
     for (var term in terms) {
-      _data.remove(term);
+      word.remove(term);
     }
   }
 }
@@ -62,12 +60,10 @@ void main() {
   Dictionary myDictionary = Dictionary();
 
   myDictionary.add("apple", "A fruit with a red or green skin.");
-  myDictionary.add("banana", "A fruit with a yellow skin.");
-  myDictionary.add("cat", "A small domesticated carnivorous mammal.");
 
   print(myDictionary.get("apple"));
 
-  myDictionary.update("banana", "A long curved fruit that grows in bunches.");
+  myDictionary.update("apple", "A long curved fruit that grows in bunches.");
 
   myDictionary.showAll();
 
@@ -85,7 +81,7 @@ void main() {
 
   myDictionary.showAll();
 
-  myDictionary.bulkDelete(["apple", "cat"]);
+  myDictionary.bulkDelete(["apple", "dog"]);
 
   myDictionary.showAll();
 }
